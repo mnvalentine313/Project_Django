7f96c7342c0a41d44e6b9d8e86017223260f8b90
Melaina Valentine <emme313@Melainas-MacBook-Pro.local>
2019-11-06 14:24:28 -0800

    Added embed_video functionality

--- youtubify/admin.py
+++ youtubify/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .models import User, Video
 # Register your models here.
 admin.site.register([User, Video])
+
+# Embed_Videos functionality resource: https://pypi.org/project/django-embed-video/
--- youtubify/models.py
+++ youtubify/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from embed_video.fields import EmbedVideoField
 
 class User(models.Model):
     username = models.CharField(max_length=100)
@@ -10,7 +11,7 @@ class User(models.Model):
 class Video(models.Model):
     song = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
-    video_url = models.TextField()
+    video_url = EmbedVideoField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='videos')
 
     def __str__(self):
